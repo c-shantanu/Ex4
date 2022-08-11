b26e300cc33d23aabd23ed81131ce327af51457a
c-shantanu <itsmeshantranuchauhan@gmail.com>
2022-08-11 13:29:12 +0200

    Ex4

--- exercise.py
+++ exercise.py
@@ -39,6 +39,14 @@ if num > 1:
 
 # Task 5 FizzBuzz
 
-
+for i in range(1, 101):
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
 
 
